77babf1365ba8ae9d696cd3f5e0ff50bf6cdbd33
Ayrianna Chatlein <aechatlein@gmail.com>
2024-03-20 10:51:25 +0100

    Add item state functionality to BacklogItem

--- AvansDevOps-11/ItemStates/ReadyForTestingItemState.cs
+++ AvansDevOps-11/ItemStates/ReadyForTestingItemState.cs
@@ -5,8 +5,43 @@ using System.Threading.Tasks;
 
 namespace AvansDevOps_11.ItemStates
 {
-    public class ReadyForTestingItemState
+    public class ReadyForTestingItemState : IItemState
     {
+        private BacklogItem Item;
         
+        public ReadyForTestingItemState(BacklogItem item)
+        {
+            this.Item = item;
+        }
+
+        public void Start()
+        {
+           Console.WriteLine("Item is already started");
+        }
+        public void Finish()
+        {
+            Console.WriteLine("Item should be tested first");
+        }
+        public void Test()
+        {
+            Console.WriteLine("Moving item to 'Testing'");
+            this.Item.ItemState = new TestingItemState(this.Item);
+        }
+        public void FinishTest()
+        {
+            Console.WriteLine("Item should be tested first");
+        }
+        public void Redo()
+        {
+            Console.WriteLine("Item can't be redone, it's already started");
+        }
+        public void Retest()
+        {
+            Console.WriteLine("Item has not been tested yet");
+        }
+        public void Done()
+        {
+            Console.WriteLine("Item has not been tested nor checked yet");
+        }
     }
 }
--- AvansDevOps-11/ItemStates/TestedItemState.cs
+++ AvansDevOps-11/ItemStates/TestedItemState.cs
@@ -5,8 +5,44 @@ using System.Threading.Tasks;
 
 namespace AvansDevOps_11.ItemStates
 {
-    public class TestedItemState
+    public class TestedItemState : IItemState
     {
+        private BacklogItem Item;
+
+        public TestedItemState(BacklogItem item)
+        {
+            this.Item = item;
+        }
         
+        public void Start()
+        {
+            Console.WriteLine("Item is already tested");
+        }
+        public void Finish()
+        {
+            Console.WriteLine("Item is already tested");
+        }
+        public void Test()
+        {
+            Console.WriteLine("Item is already tested");
+        }
+        public void FinishTest()
+        {
+            Console.WriteLine("Item is already tested");
+        }
+        public void Redo()
+        {
+            Console.WriteLine("Item can't be redone, it's already tested");
+        }
+        public void Retest()
+        {
+            Console.WriteLine("Moving item back to 'Ready for testing'");
+            this.Item.ItemState = new ReadyForTestingItemState(this.Item);
+        }
+        public void Done()
+        {
+            Console.WriteLine("Moving item to 'Done'");
+            this.Item.ItemState = new DoneItemState(this.Item);
+        }
     }
 }
