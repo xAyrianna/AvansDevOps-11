256d69a6f471065d64ce7072ef085d423858d0a4
Ayrianna Chatlein <aechatlein@gmail.com>
2024-03-20 11:30:29 +0100

    Moved classes to folder

--- AvansDevOps-11/ExportStrategy/IExportStrategy.cs
+++ AvansDevOps-11/ExportStrategy/IExportStrategy.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AvansDevOps_11
+namespace AvansDevOps_11.ExportStrategy
 {
-    public class PipelineComposite
+    public interface IExportStrategy
     {
         
     }
--- AvansDevOps-11/Pipeline/Pipeline.cs
+++ AvansDevOps-11/Pipeline/Pipeline.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AvansDevOps_11
+namespace AvansDevOps_11.Pipeline
 {
-    public class PipelineCommand
+    public class Pipeline
     {
         
     }
