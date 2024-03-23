c7fc1345673e44c24f3329d6dcb04d41bbcffc97
Ayrianna Chatlein <aechatlein@gmail.com>
2024-03-23 18:22:05 +0100

    Add Tester class and properties to User class

--- AvansDevOps-11/Users/User.cs
+++ AvansDevOps-11/Users/User.cs
@@ -7,6 +7,10 @@ namespace AvansDevOps_11.Users
 {
     public abstract class User
     {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string SlackUsername { get; set; }
 
     }
 }
