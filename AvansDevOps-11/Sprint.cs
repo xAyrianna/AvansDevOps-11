c7a9471dd6a6fa0dc016dd5feb350a02f8191ab9
Ayrianna Chatlein <aechatlein@gmail.com>
2024-03-23 18:22:11 +0100

    Refactor Sprint class properties and methods

--- AvansDevOps-11/Sprint.cs
+++ AvansDevOps-11/Sprint.cs
@@ -13,15 +13,19 @@ namespace AvansDevOps_11
     public abstract class Sprint
     {
         public Project Project;
-        public List<Developer> developers = new();
-        public ScrumMaster ScrumMaster { get; set; }
+        public string Name { get { return Name; } set { if (State is CreatedSprintState) { Name = value; } else { Console.WriteLine("Can no longer change sprint name."); } } }
+        public DateTime StartDate { get { return StartDate; } set { if (State is CreatedSprintState) { StartDate = value; } else { Console.WriteLine("Can no longer change start date."); } } }
+        public DateTime EndDate { get { return EndDate; } set { if (State is CreatedSprintState) { EndDate = value; } else { Console.WriteLine("Can no longer change end date."); } } }
+        public List<BacklogItem> BacklogItems = new();
+        public List<Developer> Developers = new();
+        public ScrumMaster ScrumMaster;
         public ISprintState State { get; set; }
         public Pipeline? Pipeline { get; set; }
-        public bool Review { get; set; }
+        public bool Review;
         public Document? ReviewSummary { get; set; }
 
         public NotificationEvent NotificationEvent { get; set; }
-        public string Name { get; set; }
+
 
         public Sprint(Project project, ScrumMaster scrumMaster)
         {
@@ -30,9 +34,51 @@ namespace AvansDevOps_11
             State = new CreatedSprintState(this);
 
             NotificationEvent = new NotificationEvent();
-            NotificationEvent.Subscribe(new EmailAdapterStrategy());
-            NotificationEvent.Subscribe(new SMSAdapterStrategy());
             NotificationEvent.Subscribe(new SlackAdapterStrategy());
+        }
+
+        public void AddNotificationStrategy(INotificationAdapterStrategy strategy)
+        {
+            NotificationEvent.Subscribe(strategy);
+        }
+
+        public void RemoveNotificationStrategy(INotificationAdapterStrategy strategy)
+        {
+            NotificationEvent.Unsubscribe(strategy);
+        }
+
+        public void AddBacklogItem(BacklogItem backlogItem)
+        {
+            if (State is CreatedSprintState)
+            {
+                BacklogItems.Add(backlogItem);
+            }
+            else
+            {
+                Console.WriteLine("Can no longer add backlog items to this sprint.");
+            }
+        }
+
+        public void RemoveBacklogItem(BacklogItem backlogItem)
+        {
+            if (State is CreatedSprintState)
+            {
+                BacklogItems.Remove(backlogItem);
+            }
+            else
+            {
+                Console.WriteLine("Can no longer remove backlog items from this sprint.");
+            }
+        }
+
+        public void AddDeveloper(Developer developer)
+        {
+            Developers.Add(developer);
+        }
+
+        public void RemoveDeveloper(Developer developer)
+        {
+            Developers.Remove(developer);
         }
     }
 }
