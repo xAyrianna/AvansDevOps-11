6edc4170cc7133067935731e1b4343e31221070b
Ayrianna Chatlein <aechatlein@gmail.com>
2024-03-23 18:21:53 +0100

    Add ReleaseSprint and ReviewSprint classes with constructors

--- AvansDevOps-11/ReleaseSprint.cs
+++ AvansDevOps-11/ReleaseSprint.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AvansDevOps_11.Users;
 
 namespace AvansDevOps_11
 {
-    public class ReleaseSprint
+    public class ReleaseSprint : Sprint
     {
-        
+        public ReleaseSprint(Project project, ScrumMaster scrumMaster) : base(project, scrumMaster)
+        {   
+            this.Review = false;
+        }
     }
 }
--- AvansDevOps-11/ReviewSprint.cs
+++ AvansDevOps-11/ReviewSprint.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AvansDevOps_11.Users;
 
 namespace AvansDevOps_11
 {
-    public class ReviewSprint
+    public class ReviewSprint : Sprint
     {
-        
+        public ReviewSprint(Project project, ScrumMaster scrumMaster) : base(project, scrumMaster)
+        {
+            this.Review = true;
+        }
     }
 }
